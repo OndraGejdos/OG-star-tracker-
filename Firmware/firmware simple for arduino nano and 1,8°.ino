c24ebda764c52855379feb93a3036ce160352a06
OndraGejdos <109963574+OndraGejdos@users.noreply.github.com>
2023-02-15 21:26:47 +0100

    Update firmware simple for arduino nano and 1,8°.ino

--- Firmware/firmware simple for arduino nano and 1,8°.ino
+++ Firmware/firmware simple for arduino nano and 1,8°.ino
@@ -1,26 +1,23 @@
 const int EN=2;         //ENABLE PIN
 const int Step=3;       // STEP PIN
 const int dir=4;        // DIRECTION PIN
-#define button 11 ;
-boolean buttonState;
 void setup()
 {
 pinMode(EN,OUTPUT);     // ENABLE AS OUTPUT
 pinMode(dir,OUTPUT);    // DIRECTION AS OUTPUT
 pinMode(Step,OUTPUT);   // STEP AS OUTPUT
 digitalWrite(dir,HIGH);
-pinMode(button,INPUT_PULLUP);
 }
 void loop()
 {
 
-buttonState = digitalRead(button)
+
 digitalWrite(dir,HIGH);
-digitalWrite(Step,LOW);    // STEP HIGH
-delay(132.91 );                   // WAIT
-digitalWrite(Step,HIGH);     // STEP LOW
-delay(132.91 );                   // WAIT
+digitalWrite(Step,HIGH);    // STEP HIGH
+delay(132.91);                   // WAIT
+digitalWrite(Step,LOW);     // STEP LOW
+delay(132.91);                   // WAIT
 
 
 }
-for(int )
+
