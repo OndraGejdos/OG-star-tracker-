91ad85cbbc46c7d27fdc298829ef60c969383855
Jugal Jesing <50669199+jugal2001@users.noreply.github.com>
2023-06-01 22:27:19 +0530

    Create config.h

---
+++ esp32_wireless_control/firmware/config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG
+#define CONFIG
+
+/*****USER DEFINED*****/
+//AP mode by default: ESP32 will create a wifi network which you can connect to
+#define AP              //comment this line if you want ESP32 to connect to your existing wifi network/hotspot
+#define c_DIRECTION 1   //set to '0' if tracking direction is reversed, TBD need to add support for southern hemisphere
+#define STEPPER_0_9     //uncomment this line if you have a 0.9 degree NEMA17
+//#define STEPPER_1_8   //uncomment this line if you have a 1.8 degree NEMA17, and comment the above line
+const uint32_t c_SLEW_SPEED = 500;    //tweak this value if you want to increase/decrease the max slew speed, change in steps of 100
+/**********************/
+
+/*****DO NOT MODIFY BELOW*****/
+//LEDs for intervalometer status and general purpose status led
+#define INTERV_PIN    25
+#define STATUS_LED    26
+
+//Stepper driver pins -- intended for TMC2209 for now
+//AXIS 1 - RA
+#define AXIS1_STEP     5
+#define AXIS1_DIR     15
+#define SPREAD_1       4
+//AXIS 2 - DEC
+#define AXIS2_STEP    19
+#define AXIS2_DIR     18
+#define SPREAD_2      21
+//common pins
+#define MS1           23
+#define MS2           22
+#define EN12_n        17
+
+#endif
