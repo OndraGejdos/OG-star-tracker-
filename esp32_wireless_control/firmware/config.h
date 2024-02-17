6cac7579af8b5775974217e29b59b078a19f617e
Jugal Jesing <50669199+jugal2001@users.noreply.github.com>
2024-02-17 15:08:19 +0530

    Update config.h

--- esp32_wireless_control/firmware/config.h
+++ esp32_wireless_control/firmware/config.h
@@ -7,7 +7,7 @@
 #define c_DIRECTION 1   //1 is for north hemisphere and 0 for south hemisphere
 #define STEPPER_0_9     //uncomment this line if you have a 0.9 degree NEMA17
 //#define STEPPER_1_8   //uncomment this line if you have a 1.8 degree NEMA17, and comment the above line
-const uint32_t c_SLEW_SPEED = 200;    //tweak this value if you want to increase/decrease the max slew speed, change in steps of 100
+const uint32_t SLEW_SPEED = 200;    //tweak this value if you want to increase/decrease the max slew speed, change in steps of 100
 /**********************/
 
 /*****DO NOT MODIFY BELOW*****/
